5c7b8ec6e8bb0b3b0b649e5e46c3a2bd51945b42
Vitaly Petrov <vitaly.petrov@softgamings.com>
2022-02-21 12:24:50 +0300

    Задание №4

---
+++ MyFirstTest1/ContactCreationTests.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private string baseURL = "http://localhost/addressbook/";
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        [Test]
+        
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToAddContact();
+            ContactForm contact = new ContactForm(
+                "Vitya",
+                "Petrovich",
+                "Bumaga",
+                "Greshnik",
+                "Transription some",
+                "ООО 'ПсковскийХлобокомбинат'",
+                "ulica Bezumiya",
+                "69",
+                "8-951-998-95-96",
+                "01-01-00",
+                "01-01-00",
+                "Uragantest666+1@gmail.com",
+                "Uragantest666+2@gmail.com",
+                "Uragantest666+3@gmail.com",
+                "www.leningrad.ru",
+                "3",
+                "April",
+                "1991",
+                "Venigret street",
+                "55",
+                "Ну и тут некое описание");
+            ContactCreation(contact);
+            BackToHomePage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.CssSelector("form[name='logout'] a")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.CssSelector("input[name='user']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='user']")).SendKeys(account.Username);
+            driver.FindElement(By.CssSelector("input[name='pass']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='pass']")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+        private void GoToAddContact()
+        {
+            // Тут возможно привязка только к ссылке
+            driver.FindElement(By.CssSelector("a[href='edit.php']")).Click();
+        }
+
+        /// <summary>
+        /// Метод заполнения всех полей при создании контакта
+        /// </summary>
+        /// <param name="contact"></param>
+        private void ContactCreation(ContactForm contact)
+        {
+            driver.FindElement(By.CssSelector("input[name='firstname']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='firstname']")).SendKeys(contact.FirstName);
+            driver.FindElement(By.CssSelector("input[name='middlename']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='middlename']")).SendKeys(contact.MiddleName);
+            driver.FindElement(By.CssSelector("input[name='lastname']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='lastname']")).SendKeys(contact.LastName);
+            driver.FindElement(By.CssSelector("input[name='nickname']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='nickname']")).SendKeys(contact.NickName);
+            driver.FindElement(By.CssSelector("input[name='title']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='title']")).SendKeys(contact.Title);
+            driver.FindElement(By.CssSelector("input[name='company']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='company']")).SendKeys(contact.Company);
+            driver.FindElement(By.CssSelector("textarea[name='address']")).Clear();
+            driver.FindElement(By.CssSelector("textarea[name='address']")).SendKeys(contact.Address);
+            driver.FindElement(By.CssSelector("input[name='home']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='home']")).SendKeys(contact.THome);
+            driver.FindElement(By.CssSelector("input[name='mobile']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='mobile']")).SendKeys(contact.TMobile);
+            driver.FindElement(By.CssSelector("input[name='work']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='work']")).SendKeys(contact.TWork);
+            driver.FindElement(By.CssSelector("input[name='fax']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='fax']")).SendKeys(contact.TFax);
+            driver.FindElement(By.CssSelector("input[name='email']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='email']")).SendKeys(contact.Email);
+            driver.FindElement(By.CssSelector("input[name='email2']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='email2']")).SendKeys(contact.Email2);
+            driver.FindElement(By.CssSelector("input[name='email3']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='email3']")).SendKeys(contact.Email3);
+            driver.FindElement(By.CssSelector("input[name='homepage']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='homepage']")).SendKeys(contact.Homepage);
+
+            //Заполнение селектора даты рождения
+            driver.FindElement(By.CssSelector("form[name='theform']")).Click();
+            driver.FindElement(By.CssSelector("select[name='bday']")).Click();
+            new SelectElement(driver.FindElement(By.CssSelector("select[name='bday']"))).SelectByText("3");
+            driver.FindElement(By.CssSelector("select[name='bmonth']")).Click();
+            new SelectElement(driver.FindElement(By.CssSelector("select[name='bmonth']"))).SelectByText("April");
+            driver.FindElement(By.CssSelector("input[name='byear']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='byear']")).SendKeys("1993");
+
+            // Селектор Anniversary
+            driver.FindElement(By.CssSelector("select[name='aday']")).Click();
+            new SelectElement(driver.FindElement(By.CssSelector("select[name='aday']"))).SelectByText("1");
+            driver.FindElement(By.CssSelector("select[name='amonth']")).Click();
+            new SelectElement(driver.FindElement(By.CssSelector("select[name='amonth']"))).SelectByText("March");
+            driver.FindElement(By.CssSelector("input[name='ayear']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='ayear']")).SendKeys("2008");
+
+            // Селектор выбора группы
+            driver.FindElement(By.CssSelector("select[name='new_group']")).Click();
+            new SelectElement(driver.FindElement(By.CssSelector("select[name='new_group']"))).SelectByText("Lol");
+
+            driver.FindElement(By.CssSelector("textarea[name='address2']")).Clear();
+            driver.FindElement(By.CssSelector("textarea[name='address2']")).SendKeys(contact.SAddress);
+            driver.FindElement(By.CssSelector("input[name='phone2']")).Clear();
+            driver.FindElement(By.CssSelector("input[name='phone2']")).SendKeys(contact.SHome);
+            driver.FindElement(By.CssSelector("textarea[name='notes']")).Clear();
+            driver.FindElement(By.CssSelector("textarea[name='notes']")).SendKeys(contact.SNotes);
+        }
+
+        private void BackToHomePage()
+        {
+            // Тут возможно привязка только к ссылке
+            driver.FindElement(By.CssSelector("a[href='./']")).Click();
+        }
+    }
+}
---
+++ MyFirstTest1/ContactForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactForm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string NickName { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string THome { get; set; }
+        public string TMobile { get; set; }
+        public string TWork { get; set; }
+        public string TFax { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string Homepage { get; set; }
+
+        // Селекторы дня рождения
+        public string BDay { get; set; }
+        public string BMonth { get; set; }
+        public string BYear { get; set; }
+
+        // Secondary
+        public string SAddress { get; set; }
+        public string SHome { get; set; }
+        public string SNotes { get; set; }
+
+        public ContactForm(
+            string firstname,
+            string middleName,
+            string lastname,
+            string nickName,
+            string title,
+            string company,
+            string address,
+            string tHome,
+            string tMobile,
+            string tWork,
+            string tFax,
+            string email,
+            string email2,
+            string email3,
+            string homepage,
+            // Селекторы дня рождения
+            string bDay,
+            string bMonth,
+            string bYear,
+            // Secondary
+            string sAddress,
+            string sHome,
+            string sNotes
+            )
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            MiddleName = middleName;
+            NickName = nickName;
+            Title = title;
+            Company = company;
+            Address = address;
+            THome = tHome;
+            TMobile = tMobile;
+            TWork = tWork;
+            TFax = tFax;
+            Email = email;
+            Email2 = email2;
+            Email3 = email3;
+            Homepage = homepage;
+            // Селекторы дня рождения
+            BDay = bDay;
+            BMonth = bMonth;
+            BYear = bYear;
+            // Secondary
+            SAddress = sAddress;
+            SHome = sHome;
+            SNotes = sNotes;
+        }
+    }
+}
